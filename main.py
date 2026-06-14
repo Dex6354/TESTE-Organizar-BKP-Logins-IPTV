b6f15c0e175426ed4cb7c4a3397489bc6f0d4c31
Dex6354 <dex.teste@proton.me>
2026-06-14 13:55:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
 import requests
 import streamlit as st
 
-urls = [
-    "https://websmt.ca",
-    "https://websmt.ca/player_api.php?username=concmus03&password=3a3b3c3d"
-]
+ip = requests.get("https://api.ipify.org").text
 
-for url in urls:
-
-    st.write("###", url)
-
-    try:
-        r = requests.get(
-            url,
-            timeout=30,
-            allow_redirects=True
-        )
-
-        st.write("Status:", r.status_code)
-
-        st.write("Server:", r.headers.get("Server"))
-
-        st.text(r.text[:500])
-
-    except Exception as e:
-        st.exception(e)
+st.write("IP Público:")
+st.code(ip)
